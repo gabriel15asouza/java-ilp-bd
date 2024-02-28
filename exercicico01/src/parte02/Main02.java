9475a288750d8b4697185ee2368e118d9e04564e
gabriel15asouza <gabalves.souza@gmail.com>
2024-02-28 08:41:50 -0300

    [feat] - conclusao exercicicio01

---
+++ exercicico01/src/parte02/Main02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parte02;
+
+/**
+ *
+ * @author gabal
+ */
+public class Main02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá mundo!");
+        System.out.println("Jesus é bom!");
+    }
+    
+}
